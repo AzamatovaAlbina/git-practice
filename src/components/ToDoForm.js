dd2145e36533dbb4fee84eea4d3ac4bbb4de4230
Albina <azamatovaalbina7@gmail.com>
2022-10-12 17:40:15 +0600

    todos

---
+++ src/components/ToDoForm.js
@@ -0,0 +1,30 @@
+import { useState } from "react";
+function ToDoForm({addTask}) {
+  const [userInput, setUserInput] = useState("");
+  const handleSubmit = (e) => {
+      e.preventDefault();
+      addTask(userInput)
+      setUserInput("")
+  };
+  const handleChange = (e) => {
+    setUserInput(e.currentTarget.value)
+  };
+  const handleKeyPress = (e) => {
+  if (e.key==="Enter") {
+    handleSubmit(e)
+  }
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        value={userInput}
+        type="text"
+        onChange={handleChange}
+        onKeyDown={handleKeyPress}
+        placeholder="Enter text..."
+      />
+      <button>Save</button>
+    </form>
+  );
+}
+export default ToDoForm;
